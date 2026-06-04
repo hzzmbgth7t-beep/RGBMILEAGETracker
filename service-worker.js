28e5f425a98f7067242dada3105e96dbceef85cf
hzzmbgth7t-beep <hzzmbgth7t@privaterelay.appleid.com>
2026-06-03 21:59:47 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='rgbm-v1.1.0-2026-06-03';
+const CACHE_NAME='rgbm-v2.0.7-2026-06-03';
 const FILES=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-180.png','./icon-192.png','./icon-512.png','./favicon-32x32.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
